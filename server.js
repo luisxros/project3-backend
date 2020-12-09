e69e2214269d2752f3b5e0b8ffa95b24578e4ca6
Luis Rosario <luisalmonter@gmail.com>
2020-12-08 23:22:32 -0500

    updated for users only

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 
 require('dotenv').config();
 
-const scoresRouter = require('./routes/api/scores');
+// const scoresRouter = require('./routes/api/scores');
 const usersRouter = require('./routes/api/users');
 
 
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(cors());
 
 
-app.use('/api/scores', scoresRouter);
+// app.use('/api/scores', scoresRouter);
 app.use('/api/users', usersRouter);
 
 const port = process.env.PORT || 3001;
